a48fd7eda8c57ed1df1d49f216c12ea18ef26fe4
suxun <871728578@qq.com>
2017-08-31 20:01:36 +0800

    update logic.js

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,41 +1,57 @@
 var beginOrContinueInput = true;
 var resetInputAsFocusForTheFirstTimeState = true;
- 
-function isNullCharacter(){
+
+function isNullCharacter() {
     var value = $('input#query').val();
-    return value === "" ? true : false; 
- }
-function reset(){
+    return value === "" ? true : false;
+}
+
+function reset() {
     $("svg.clear").removeClass('show').addClass('hide')
     $("label.holder").removeClass('hide').addClass('show')
 }
-function inputing(){
-     $("svg.clear").removeClass("hide").addClass('show')
-     $("label.holder").removeClass('show').addClass('hide')
+
+function inputing() {
+    $("svg.clear").removeClass("hide").addClass('show')
+    $("label.holder").removeClass('show').addClass('hide')
 }
-$('input#query').on('focus',function(e){
-    if(resetInputAsFocusForTheFirstTimeState){
-    reset()
-    resetInputAsFocusForTheFirstTimeState = false;
+
+$('.tabs>ul').on('click', 'li', function (e) {
+    let li = $(e.currentTarget);
+    let index = li.index();
+    li.addClass('active').siblings('.active').removeClass('active');
+    $('.tab-contents>ol>li').eq(index).addClass('active').siblings('.active').removeClass('active');
+    if (index == 2 && isNullCharacter()) {
+        $('input#query').focus()
     }
 })
-$('input#query').on('input',function(e){
-    if(beginOrContinueInput){
-     inputing()
-     beginOrContinueInput = false;
+
+$('input#query').on('focus', function (e) {
+    if (resetInputAsFocusForTheFirstTimeState) {
+        reset()
+        resetInputAsFocusForTheFirstTimeState = false;
     }
-    if(isNullCharacter()){
+})
+
+$('input#query').on('input', function (e) {
+    if (beginOrContinueInput) {
+        inputing()
+        beginOrContinueInput = false;
+    }
+    if (isNullCharacter()) {
         reset()
         beginOrContinueInput = true;
     }
-     let $input = $(e.currentTarget)
+    let $input = $(e.currentTarget)
     // let $input = $(this);
     let value = $input.val()
 })
-$('input#query').on('blur',function(){
-     beginOrContinueInput = true;
+
+$('input#query').on('blur', function () {
+    beginOrContinueInput = true;
 })
-$('svg.clear').on('click',function(e){
+
+$('svg.clear').on('click', function (e) {
     $('input#query').val('')
     resetInputAsFocusForTheFirstTimeState = true;
     $('input#query').focus();
