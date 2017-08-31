f4083db3e46d34785f15008f26725317946390ee
suxun <871728578@qq.com>
2017-08-31 16:34:11 +0800

    update logic.js

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,24 +1,42 @@
-var hide = true;
+var beginOrContinueInput = true;
+var resetInputAsFocusForTheFirstTimeState = true;
+ 
 function isNullCharacter(){
     var value = $('input#query').val();
     return value === "" ? true : false; 
  }
-$('input#query').on('focus',function(e){
+function reset(){
     $("svg.clear").removeClass('show').addClass('hide')
     $("label.holder").removeClass('hide').addClass('show')
+}
+function inputing(){
+     $("svg.clear").removeClass("hide").addClass('show')
+     $("label.holder").removeClass('show').addClass('hide')
+}
+$('input#query').on('focus',function(e){
+    if(resetInputAsFocusForTheFirstTimeState){
+    reset()
+    resetInputAsFocusForTheFirstTimeState = false;
+    }
 })
 $('input#query').on('input',function(e){
-    if(hide || isNullCharacter()){
-     $("svg.clear").removeClass('hide').addClass('show')
-     $("label.holder").removeClass('hide').addClass('hide')
-     hide = false;
+    if(beginOrContinueInput){
+     inputing()
+     beginOrContinueInput = false;
+    }
+    if(isNullCharacter()){
+        reset()
+        beginOrContinueInput = true;
     }
      let $input = $(e.currentTarget)
     // let $input = $(this);
     let value = $input.val()
 })
+$('input#query').on('blur',function(){
+     beginOrContinueInput = true;
+})
 $('svg.clear').on('click',function(e){
     $('input#query').val('')
+    resetInputAsFocusForTheFirstTimeState = true;
     $('input#query').focus();
-    hide = true;
 })
