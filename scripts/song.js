99bca8ffff5c89dec9e2d9c19c29527e2a795d7a
suxun <871728578@qq.com>
2017-09-08 04:06:10 +0800

    dynamic generator html

--- scripts/song.js
+++ scripts/song.js
@@ -24,7 +24,9 @@ function parseLyric(lyric) {
         let min = parseInt(timeArr[0].split(':')[0])
         let sec = parseInt(timeArr[0].split(':')[1].split(".")[0])
         let time = min * 60 + sec;
-        lrcObj[time] = clause;
+        if (clause) {
+            lrcObj[time] = clause;
+        }
     }
     for (let key of Object.keys(lrcObj)) {
         let li = `
@@ -42,20 +44,43 @@ function parseLyric(lyric) {
         // let {url,lyric} = song.attributes;
         var url = song.attributes.url;
         var lyric = song.attributes.lyric
+        var coverImgURL = song.attributes.cover;
+        var name = song.attributes.name;
+        var singer = song.attributes.singer;
+        let li = `
+             <h2>
+                <span>${name}</span>
+                <span class="mid">-</span>
+                <b>${singer}</b>
+            </h2>
+        `
+        $('.lyric').prepend(li)
+        $(`<style>.page::before { background:url(${coverImgURL}) no-repeat center;background-size:"auto 100%" }</style>`).appendTo("head")
+        var img = document.createElement('img')
+        img.src = coverImgURL;
+        document.querySelector('.circle').appendChild(img)
+        $(img).attr('class', 'cover')
         lrcObj = parseLyric(lyric)
-        var height = $('.lyricScroll p').height()+8;
+        var height = $('.lyricScroll p').height() + 8;
         var video = document.createElement('video')
         video.src = url;
         document.head.appendChild(video)
+        $('video')[0].addEventListener('ended', function (e) {
+            $('.circle').removeClass('playing')
+            $('svg.play').removeClass('hide').addClass('show');
+            $('.lyricScroll').css({
+                transform: 'translateY(0)'
+            })
+        })
         $('video')[0].addEventListener('timeupdate', function (e) {
             var crtTime = parseInt($('video')[0].currentTime);
             if (lrcObj[crtTime]) {
-                let index = Object.keys(lrcObj).indexOf(crtTime.toString())-1;
-                index = index < 0 ? 0 :index;
+                let index = Object.keys(lrcObj).indexOf(crtTime.toString()) - 1;
+                index = index < 0 ? 0 : index;
                 $('.lyricScroll').css({
                     transform: 'translateY(' + (-height * index) + 'px)'
                 })
-                $('.lyricScroll p').eq(index+1).addClass('highLight').siblings('.highLight').removeClass('highLight')
+                $('.lyricScroll p').eq(index + 1).addClass('highLight').siblings('.highLight').removeClass('highLight')
             }
         })
     })
@@ -65,9 +90,18 @@ $('svg.play').on('click', function (e) {
     $('.circle').removeClass('pause').addClass('playing')
     $('svg.play').addClass('hide')
 })
-$('img.cover').on('click', function (e) {
+// $('img.cover').on('click', function (e) {
+//     $('video')[0].pause();
+//     $('svg.play').removeClass('hide').addClass('show');
+//     $('.circle').addClass('pause')
+
+// })
+$('.circle').on('click', 'img.cover', function (e) {
     $('video')[0].pause();
     $('svg.play').removeClass('hide').addClass('show');
     $('.circle').addClass('pause')
 
 })
+// $('body').on('click',function(e){
+//     console.log(e.target)
+// })
