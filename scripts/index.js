ba74e93ce2272f8592131003504835ef2e8e8e3c
suxun <871728578@qq.com>
2017-09-14 02:44:23 +0800

    finish page2

--- scripts/index.js
+++ scripts/index.js
@@ -14,9 +14,9 @@
  let $ol = $('.newSongs>ol')
  query.find().then(function (results) {
      $('#loading').remove();
+     results = getElementsInArray(results, 10);
      for (var i = 0; i < results.length; i++) {
          let song = results[i].attributes;
-         console.log(results[i])
          let li = `<li>
                         <a href='./song.html?id=${results[i].id}'>
                                 <div class="songInfo">
@@ -45,6 +45,43 @@
              $('.newSongs>ol>li').eq(i).find('.singer').prepend(liSQ)
          }
      }
+     results = quickSort(results).reverse();
+     for (let j = 0; j < results.length; j++) {
+         let song = results[j].attributes;
+         let index = convertNum(j + 1)
+         let li = `
+                    <li>
+                        <a href="./song.html?id=${results[j].id}">
+                            <div class="rank">${index}</div>
+                                <div class="songInfo">
+                                    <div class="songName">
+                                        <p>${song.name}</p>
+                                    </div>
+                                    <div class="singer">
+                                        ${song.singer}-${song.album}
+                                    </div>
+                                </div>
+                                <div class="play">
+                                    <svg class="icon">
+                                        <use xlink:href="#icon-play"></use>
+                                    </svg>
+                                </div>
+                        </a>
+                    </li>
+     `
+         $('.hotSongList>ol').append(li)
+         let liSQ = `
+                        <svg class="icon">
+                            <use xlink:href="#icon-SQ"></use>
+                        </svg>
+         `
+         if (song.highquality) {
+             $('.hotSongList>ol>li').eq(j).find('.singer').prepend(liSQ)
+         }
+         if (j < 3) {
+             $('.hotSongList .rank').eq(j).addClass('hotSong')
+         }
+     }
  }, function (error) {
      throw new Error(error)
  })
@@ -69,11 +106,12 @@
          return arr;
      }
      var temp = arr.pop();
+     var tempindex = temp.attributes.popularindex;
      var smaller = arr.filter(function (ele) {
-         return ele <= temp
+         return ele.attributes.popularindex <= tempindex
      })
      var greater = arr.filter(function (ele) {
-         return ele > temp;
+         return ele.attributes.popularindex > tempindex;
      })
      return quickSort(smaller).concat([temp]).concat(quickSort(greater))
 
